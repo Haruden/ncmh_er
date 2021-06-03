0933857922f4391ed5d6e6b5518efd321f5279f3
Haruden <75109038+Haruden@users.noreply.github.com>
2021-06-03 08:54:19 +0800

    Minor edit

--- application/views/nurse/queue/sidemenu.php
+++ application/views/nurse/queue/sidemenu.php
@@ -1,5 +1,6 @@
 <div class="card card-primary">
     <div class="card-body">
+        
         <?php if($title== "Patient Details"){ ?>
             <div style="background-color:#7A3B69;" class="p-2 rounded">
                 <a style="color:white;" href="#"> Patient Details </a>
@@ -7,7 +8,9 @@
         <?php } else { ?>
             <strong><a href="<?php echo base_url('nurse/pat_details');?>"> Patient Details </a> </strong>
         <?php } ?>
+
         <hr>
+
         <?php if($title== "Vital Signs"){ ?>
             <div style="background-color:#7A3B69;" class="p-2 rounded">
                 <a style="color:white;" href="#"> Vital Signs </a>
@@ -15,7 +18,9 @@
         <?php } else { ?>
             <strong><a href="<?php echo base_url('nurse/vitals');?>"> Vital Signs </a> </strong>
         <?php } ?>
+
         <hr>
+
         <?php if($title== "Patient ID Marks"){ ?>
             <div style="background-color:#7A3B69;" class="p-2 rounded">
                 <a style="color:white;" href="#"> Patient ID Marks </a>
@@ -23,7 +28,9 @@
         <?php } else { ?>
             <strong><a href="<?php echo base_url('nurse/pat_id_marks');?>"> Patient ID Marks </a> </strong>
         <?php } ?>
+
         <hr>
+
         <?php if($title== "Nurse's Notes"){ ?>
             <div style="background-color:#7A3B69;" class="p-2 rounded">
                 <a style="color:white;" href="#"> Nurse's Notes </a>
@@ -31,6 +38,7 @@
         <?php } else { ?>
             <strong><a href="<?php echo base_url('nurse/nurses_notes');?>"> Nurse's Notes </a> </strong>
         <?php } ?>
+
     </div>
     <!-- /.card-body -->
 </div>
