0b4191cfd503d5c499ea3eb51292435cc2d0ab75
jjescartin <46393855+jjescartin@users.noreply.github.com>
2021-06-03 19:45:23 +0800

    patient-id-marks

--- application/views/nurse/queue/action_dropdown.php
+++ application/views/nurse/queue/action_dropdown.php
@@ -5,7 +5,7 @@
     <button type="button" class="dropdown-item btn btn-primary" data-toggle="modal" data-target="#ATD-modal"> Add to Doctor </button>
     <a class="dropdown-item" href="<?php echo base_url(''); ?>">Patient Details</a>
     <a class="dropdown-item" href="<?php echo base_url('nurse/vitals'); ?>">Vital Signs</a>
-    <a class="dropdown-item" href="<?php echo base_url(''); ?>">Patient ID Marks</a>
+    <a class="dropdown-item" href="<?php echo base_url('nurse/pat_id_marks'); ?>">Patient ID Marks</a>
     <a class="dropdown-item" href="<?php echo base_url(''); ?>">Nurse's Notes</a>
     <div class="dropdown-divider"></div>
     <a class="dropdown-item" href="#">Out when called</a>
