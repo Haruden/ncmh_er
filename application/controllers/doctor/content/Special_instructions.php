18ef9ef0c9d0c47df09e24b0edeb3216ca07da72
Haruden <75109038+Haruden@users.noreply.github.com>
2021-10-21 13:01:10 +0800

    Integrate Special Instruction

--- application/controllers/doctor/content/Special_instructions.php
+++ application/controllers/doctor/content/Special_instructions.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Not being used
 class Special_instructions extends MY_Controller {
 
 	/**
--- application/views/patient/plan.php
+++ application/views/patient/plan.php
@@ -25,11 +25,165 @@
                                         <hr>
                                     <?php } ?>
 
-                                    <div>
+                                    <div style="white-space: nowrap; overflow: hidden; margin: 0; padding: 0;">
+                                        <div class="float-left" style="display: inline-block;">
+                                            <h5>SPECIAL INSTRUCTIONS <span class="badge badge-pill badge-success center">3</span></h5>
+                                        </div>
+
+                                        <div class="float-right" style="display: inline-block;">
+                                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                                <label class="btn btn-sm btn-outline-info">
+                                                    <input type="radio" name="options" id="active-filter" value="ACTIVE" selected> ACTIVE
+                                                </label>
+                                                <label class="btn btn-sm btn-outline-info">
+                                                    <input type="radio" name="options" id="done-filter" value="DONE"> DONE
+                                                </label>
+                                                <label class="btn btn-sm btn-outline-info">
+                                                    <input type="radio" name="options" id="all-filter" value="ALL"> ALL
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="dataTables_wrapper dt-bootstrap4">
+                                        <table id="special_instructions_Table" class="table table-bordered table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th width="20">
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </th>
+                                                    <th>Instruction</th>
+                                                    <th width="140">Date/Time Instructed</th>
+                                                    <th width="100">Status</th>
+                                                    <th width="100">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </td>
+                                                    <td>Sample instruction 1.</td>
+                                                    <td>Jan 1, 2021 | 12:10PM <br><small><i class="small text-muted">Ordered by: Dr. Cruzada </i></small></td>
+                                                    <td><button type="button" class="btn btn-xs btn-block btn-success disabled">ACTIVE</button></td>
+                                                    <!-- <td><span class="badge badge-success center">ACTIVE</span></td> -->
+                                                    <td>
+                                                        <div class="input-group-prepend">
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action</button>
+                                                            <div class="dropdown-menu" style="">
+                                                                <a class="dropdown-item" href="#">Update</a>
+                                                                <a class="dropdown-item" href="#">Complete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </td>
+                                                    <td>Sample instruction 2.</td>
+                                                    <td>Jan 2, 2021 | 12:10PM <br><small><i class="small text-muted">Ordered by: Dr. Cruzada </i></small></td>
+                                                    <td><button type="button" class="btn btn-xs btn-block btn-success disabled">ACTIVE</button></td>
+                                                    <td>
+                                                        <div class="input-group-prepend">
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action</button>
+                                                            <div class="dropdown-menu" style="">
+                                                                <a class="dropdown-item" href="#">Update</a>
+                                                                <a class="dropdown-item" href="#">Complete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </td>
+                                                    <td>Sample instruction 3.</td>
+                                                    <td>Jan 3, 2021 | 12:10PM <br><small><i class="small text-muted">Ordered by: Dr. Cruzada </i></small></td>
+                                                    <td><button type="button" class="btn btn-xs btn-block btn-success disabled">ACTIVE</button></td>
+                                                    <td>
+                                                        <div class="input-group-prepend">
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action</button>
+                                                            <div class="dropdown-menu" style="">
+                                                                <a class="dropdown-item" href="#">Update</a>
+                                                                <a class="dropdown-item" href="#">Complete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </td>
+                                                    <td>Sample instruction 4.</td>
+                                                    <td>Jan 24, 2021 | 12:10PM <br><small><i class="small text-muted">Ordered by: Dr. Cruzada </i></small></td>
+                                                    <td><button type="button" class="btn btn-xs btn-block bg-gradient-secondary disabled">DONE</button></td>
+                                                    <td>
+                                                        <div class="input-group-prepend">
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action</button>
+                                                            <div class="dropdown-menu" style="">
+                                                                <a class="dropdown-item" href="#">Update</a>
+                                                                <a class="dropdown-item" href="#">Complete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>
+                                                        <!-- <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox">
+                                                            <label class="form-check-label"></label>
+                                                        </div> -->
+                                                    </td>
+                                                    <td>Sample instruction 5.</td>
+                                                    <td>Jan 24, 2021 | 12:10PM <br><small><i class="small text-muted">Ordered by: Dr. Cruzada </i></small></td>
+                                                    <td><button type="button" class="btn btn-xs btn-block bg-gradient-secondary disabled">DONE</button></td>
+                                                    <td>
+                                                        <div class="input-group-prepend">
+                                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action</button>
+                                                            <div class="dropdown-menu" style="">
+                                                                <a class="dropdown-item" href="#">Update</a>
+                                                                <a class="dropdown-item" href="#">Complete</a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            <tfoot>
+                                                <tr>
+                                                    <th width="20"></th>
+                                                    <th>Instruction</th>
+                                                    <th width="140">Date/Time Instructed</th>
+                                                    <th width="100">Status</th>
+                                                    <th width="100">Action</th>
+                                                </tr>
+                                            </tfoot>
+                                            </tbody>
+                                        </table>
+                                    </div> <!-- End of Table -->
+
+                                    <!-- <div>
                                         <h5>SPECIAL INSTRUCTION</h5>
                                         <textarea class="form-control" rows="2" placeholder="Special Instructions Here" disabled></textarea>
                                     </div>
-                                    <br>
+                                    <br> -->
 
                                     <h5>Doctor's Order Details</h5>
 
@@ -476,3 +630,61 @@
     </section>
     <!-- /.content -->
 </div>
+
+<script>
+    var table_alldata;
+    var filteredTable;
+
+    $(function() {
+
+        table_alldata = $('#special_instructions_Table').DataTable({
+            language: {
+                searchPlaceholder: "Search Instruction"
+            },
+            columnDefs: [{
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0
+            }],
+            select: {
+                style: 'os',
+                selector: 'td:first-child',
+            },
+            "responsive": true,
+            "autoWidth": false,
+            order: [
+                [2, 'desc']
+            ],
+            dom: 't<"bottom"p>'
+
+        });
+
+        $('#active-filter').attr('checked', true);
+        table_alldata
+            .columns(3)
+            .search('ACTIVE')
+            .draw();
+
+        $('input[name="options"]').on('change', function() {
+            var filter = $(this).val();
+            console.log('filter:' + filter);
+
+            if (filter == "ALL") {
+                table_alldata
+                    .columns()
+                    .search('')
+                    .draw();
+            } else {
+                table_alldata
+                    .columns(3)
+                    .search(filter)
+                    .draw();
+            }
+
+        });
+
+        // $("#special_instructions_Table_paginate").find('#special_instructions_Table_previous').before(
+        //     '<div class="mr-2 mt-1" style="display: inline-block;"> <div class="btn-group btn-group-toggle" data-toggle="buttons"> <label class="btn btn-sm btn-outline-info"> <input type="radio" name="options" id="active-filter" value="ACTIVE" selected> ACTIVE </label> <label class="btn btn-sm btn-outline-info"> <input type="radio" name="options" id="done-filter" value="DONE"> DONE </label> <label class="btn btn-sm btn-outline-info"> <input type="radio" name="options" id="all-filter" value="ALL"> ALL </label> </div> </div>'
+        // );
+    });
+</script>
